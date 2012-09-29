f1dc7d51af5a3e8c16b261058c544d7579c74e50
trannz <trannz@trannztop.(none)>
2012-09-29 23:27:24 +0200

    Dodany nagłówek projektowy w pliku onoff.h

--- include/hal/onoff.h
+++ include/hal/onoff.h
@@ -1,6 +1,20 @@
 #ifndef ONOFF_H_INCLUDED
 #define ONOFF_H_INCLUDED
 
+/*---------------------------------------------+
+|Name: Makefile                                |
+|  Project: alpp                               |
+|  Author: Adrian Ciesielski                   |
+|  Creation Date: 2012-08-31                   |
+|  Copyright: (c) 2012 by Adrian Ciesielski    |
+|  License: GNU LGPL v3 (see COPYING)          |
+|  Version: 0.0.1                              |
++---------------------------------------------*/
+
+
+/**
+* Enum which defines values for ON and OFF statesments.
+*/
 enum POnOff {
     ON=~0,
     OFF=0
