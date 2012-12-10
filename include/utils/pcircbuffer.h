bd8786e342bb9b7dfc47f4280330ce7a04badb37
machabeuszan <machabeuszan@o2.pl>
2012-12-10 23:35:38 +0100

    PCircBuffer został opatrzony doxygenowym opisem

--- include/utils/pcircbuffer.h
+++ include/utils/pcircbuffer.h
@@ -1,31 +1,102 @@
+/*---------------------------------------------+
+|                                              |
+| Name: include/hal/pcircbuffer.h              |
+|  Project: alpp                               |
+|  Author: Adrian Ciesielski                   |
+|  Creation Date: 2012-12-10                   |
+|  Copyright: (c) 2012 by Adrian Ciesielski    |
+|  License: GNU LGPL v3 (see COPYING)          |
+|  Version: See top of Makefile                |
+|                                              |
++---------------------------------------------*/
+
 #ifndef PCIRCBUFFER_H
 #define PCIRCBUFFER_H
 
 #include <avr/io.h>
-#include "utils/simulSerial.h"
+//#include "utils/simulSerial.h"
 
+
+/**
+ * Szablon klasy będącej jednym z kontenerów: jest to bufor cykliczny. Jego zadaniem jest buforowanie obiektów typu T.
+ * @param T jest typem który będzie przechowywany w buforze. Jeśli T jest klasą, musi mieć ona zaimplementowany operator =()
+ * @param Capacity określa pojemność bufora
+ * Bufor jest zaimplementowany tak, że przy skończniu się pojemności bufora najstarsze elementy będą nadpisywane
+ */
 template <class T, uint8_t Capacity>
 class PCircBuffer
 {
 	public:
+		/**
+		 * Publiczna definicja możliwych błędów
+		 */
 		enum ERRS {
+			/// brak błędu
 			ERROR_NO	= 0,
+			/// błąd nadpisania najstarszej danej
 			ERROR_OVERFL	= _BV(1),
+			/// błąd przy odczycie - brak danych do odczytu
 			ERROR_NOMORE	= _BV(2),
+			/// błąd oznaczający odmowę dopisania do bufora, gdyż jest pełny. Obecnie nie używany
 			ERROR_NOSPACE	= _BV(3)
 		};
+		/**
+		 * Konstruktor. Nie przyjmuje obecnie żadnych parametrów ale jest niezbędny do poprawnej inicjalizacji klasy
+		 */
 		PCircBuffer();
-		void clear();
-		void enqueue(T val);
-		//int8_t enqueue(T &rVal);
-		T dequeue();
-		//int8_t dequeue(T &rVal);
-		bool isFull();
-		bool isEmpty();
-		uint8_t size();
-		uint8_t capacity();
+		/**
+		 * Dodaje element do bufora.
+		 * @param val jest elementem który zostanie skopiowany do pamięci bufora.
+		 * Obecnie, jeśli zabraknie miejsca, najstarsze dane zostają nadpisane.
+		 */
+		void append(T val);
+		//int8_t append(T &rVal);
+		/**
+		 * Wyciąga najstarszy element z bufora i zwalnia miejsce na nowe elementy. Jako że to jest bufor cykliczny, żadne czyszczenie pamięci, ani wywoływanie destruktorów nie następuje. Zwolnione miejsce zostanie po prostu nadpisana prędej przy później przy wywołaniu append().
+		 * @return kopia najstarszego elementu z bufora.
+		 */
+		T take();
+		//int8_t take(T &rVal);
+		/**
+		 * Sprawdza czy bufor jest pełen, tj czy wszystkie miejsca są zapełnione.
+		 * @return true jeśli bufor jest pełen.
+		 */
+		bool isFull() { return (len==cap); }
+		/**
+		 * Sprawdza czy bufor jest pusty, tj nie ma w nim żadnego elementu. Uwaga !isEmpty to nie to samo co isFull().
+		 * @return true jeśli bufor jest pusty.
+		 */
+		bool isEmpty() { return (len==0); }
+		/**
+		 * Informuje ile obecnie jest elementów w buforze.
+		 * @return liczba elementów w buforze
+		 */
+		uint8_t size() { return len; }
+
+		/**
+		 * Informuje ile zostało w buforze wolnego miejsca.
+		 * @return ilość wolnego miejsca.
+		 */
+		uint8_t space() { return cap-len; }
+
+		/**
+		 * Informuje jaka jest całkowita pojemność bufora.
+		 * @return całkowita pojemność buora;
+		 */
+		uint8_t capacity() { return cap; }
+
+		/**
+		 * Pozwala sprawdzić czy operacje dopisania/odczytania powiodły się
+		 * @return kod błędu w postaci wartości z enum ERRS.
+		 * @see ERRS
+		 */
 		int8_t errors();
-		void fill(uint8_t numOfElement, T &value);
+
+		/**
+		 * Wypełnia cały bufor domyślną warotścią value
+		 * @param value wartość jaka ma się znaleźć w komórkach
+		 */
+		void fill(T &value);
 	protected:
 	private:
 		T container[Capacity];
@@ -50,29 +121,9 @@ PCircBuffer<T,Capacity>::PCircBuffer() :
 {
 }
 
-template <class T, uint8_t C>
-uint8_t PCircBuffer<T,C>::size()
-{
-	return len;
-}
 
-template <class T, uint8_t C>
-uint8_t PCircBuffer<T,C>::capacity()
-{
-	return cap;
-}
 
-template <class T, uint8_t C>
-bool PCircBuffer<T,C>::isEmpty()
-{
-	return (len==0);
-}
 
-template <class T, uint8_t C>
-bool PCircBuffer<T,C>::isFull()
-{
-	return (len==cap);
-}
 
 template <class T, uint8_t C>
 void PCircBuffer<T,C>::next(uint8_t &val)
@@ -82,7 +133,7 @@ void PCircBuffer<T,C>::next(uint8_t &val)
 
 
 template <class T, uint8_t C>
-void PCircBuffer<T,C>::enqueue(T val)
+void PCircBuffer<T,C>::append(T val)
 {
 	next(head);
 	if(isFull())
@@ -110,7 +161,7 @@ void PCircBuffer<T,C>::enqueue(T val)
 #endif
 
 template <class T, uint8_t C>
-T PCircBuffer<T,C>::dequeue()
+T PCircBuffer<T,C>::take()
 {
 	T ret;
 	if(!isEmpty())
@@ -135,6 +186,13 @@ int8_t PCircBuffer<T,C>::errors()
 	int8_t err = error_occured;
 	error_occured = ERROR_NO;
 	return err;
+}
+
+template <class T, uint8_t C>
+void PCircBuffer<T,C>::fill(T &value)
+{
+	for(uint8_t i=0; i<cap;i++)
+		container[i]=value;
 }
 
 
--- src/utils/pcircbuffer.cpp
+++
@@ -1,4 +0,0 @@
-#include "utils/pcircbuffer.h"
-
-
-
